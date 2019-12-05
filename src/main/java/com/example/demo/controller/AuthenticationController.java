d1bdf57448308d13e062bf2209fa03f7d281e5d8
Daniliva <53309093+Daniliva@users.noreply.github.com>
2019-12-05 18:07:02 +0200

    add security

---
+++ src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.config.TokenProvider;
+import com.example.demo.model.AuthToken;
+import com.example.demo.model.LoginUser;
+import com.example.demo.service.UserService;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/token")
+public class AuthenticationController {
+    Logger logger= LoggerFactory.getLogger(AuthenticationController.class);
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenProvider jwtTokenUtil;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/generate-token", method = RequestMethod.POST)
+    public ResponseEntity<?> register(@RequestBody LoginUser loginUser) throws AuthenticationException {
+try{
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginUser.getUsername(),
+                        loginUser.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        final String token = jwtTokenUtil.generateToken(authentication);
+
+
+        return ResponseEntity.ok(new AuthToken(token));}
+catch (ExpiredJwtException e)
+{
+    throw  new ExpiredJwtException(e.getHeader(),e.getClaims(), "don't"+loginUser.getUsername()+loginUser.getPassword()
+);
+
+
+}
+    }
+
+}
